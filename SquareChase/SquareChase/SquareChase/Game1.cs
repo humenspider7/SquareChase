feb774714cc5f9c67e83e545aabd34b14a9598ab
humenspider7 <iamyusuf6@aol.com>
2017-03-17 13:18:52 -0400

    Started

--- SquareChase/SquareChase/SquareChase/Game1.cs
+++ SquareChase/SquareChase/SquareChase/Game1.cs
@@ -18,6 +18,13 @@ namespace SquareChase
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Random rand = new Random();
+        Texture2D squareTexture;
+        Rectangle currentSquare;
+        int playerScore = 0;
+        float timeRemaining = 0.0f;
+        const float timePerSquare = 0.75f;
+        Color[] colors = new Color[3] { Color.Red, Color.Green, Color.Blue };
 
         public Game1()
         {
@@ -35,6 +42,7 @@ namespace SquareChase
         {
             // TODO: Add your initialization logic here
 
+            this.IsMouseVisible = true;
             base.Initialize();
         }
 
